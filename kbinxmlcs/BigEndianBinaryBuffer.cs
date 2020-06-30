999594b8d2b5e5d1d1ae0d2885554bfe745eb221
FSHBOT <matthewdipinto2@gmail.com>
2020-06-30 00:55:25 -0400

    Further LINQ removal

--- kbinxmlcs/BigEndianBinaryBuffer.cs
+++ kbinxmlcs/BigEndianBinaryBuffer.cs
@@ -30,35 +30,49 @@ namespace kbinxmlcs
 
         internal virtual void WriteS8(sbyte value) => WriteBytes(new byte[] { (byte)value });
 
-        internal virtual void WriteS16(short value) => WriteBytes(BitConverter.GetBytes(value).Reverse().ToArray());
+        internal virtual void WriteS16(short value) => WriteReverse(BitConverter.GetBytes(value));
 
-        internal virtual void WriteS32(int value) => WriteBytes(BitConverter.GetBytes(value).Reverse().ToArray());
+        internal virtual void WriteS32(int value) => WriteReverse(BitConverter.GetBytes(value));
 
-        internal virtual void WriteS64(long value) => WriteBytes(BitConverter.GetBytes(value).Reverse().ToArray());
+        internal virtual void WriteS64(long value) => WriteReverse(BitConverter.GetBytes(value));
 
         internal virtual void WriteU8(byte value) => WriteBytes(new byte[] { value });
 
-        internal virtual void WriteU16(ushort value) => WriteBytes(BitConverter.GetBytes(value).Reverse().ToArray());
+        internal virtual void WriteU16(ushort value) => WriteReverse(BitConverter.GetBytes(value));
 
-        internal virtual void WriteU32(uint value) => WriteBytes(BitConverter.GetBytes(value).Reverse().ToArray());
+        internal virtual void WriteU32(uint value) => WriteReverse(BitConverter.GetBytes(value));
 
-        internal virtual void WriteU64(ulong value) => WriteBytes(BitConverter.GetBytes(value).Reverse().ToArray());
+        internal virtual void WriteU64(ulong value) => WriteReverse(BitConverter.GetBytes(value));
 
         internal virtual sbyte ReadS8() => (sbyte)ReadBytes(sizeof(byte))[0];
 
-        internal virtual short ReadS16() => BitConverter.ToInt16(ReadBytes(sizeof(short)).Reverse().ToArray(), 0);
+        internal virtual short ReadS16() => BitConverter.ToInt16(ReadReverse(sizeof(short)), 0);
 
-        internal virtual int ReadS32() => BitConverter.ToInt32(ReadBytes(sizeof(int)).Reverse().ToArray(), 0);
+        internal virtual int ReadS32() => BitConverter.ToInt32(ReadReverse(sizeof(int)), 0);
 
-        internal virtual long ReadS64() => BitConverter.ToInt64(ReadBytes(sizeof(long)).Reverse().ToArray(), 0);
+        internal virtual long ReadS64() => BitConverter.ToInt64(ReadReverse(sizeof(short)), 0);
 
         internal virtual byte ReadU8() => ReadBytes(sizeof(byte))[0];
 
         internal virtual ushort ReadU16() => BitConverter.ToUInt16(ReadBytes(sizeof(short)).Reverse().ToArray(), 0);
 
-        internal virtual uint ReadU32() => BitConverter.ToUInt32(ReadBytes(sizeof(int)).Reverse().ToArray(), 0);
+        internal virtual uint ReadU32() => BitConverter.ToUInt32(ReadReverse(sizeof(int)), 0);
 
-        internal virtual ulong ReadU64() => BitConverter.ToUInt64(ReadBytes(sizeof(long)).Reverse().ToArray(), 0);
+        internal virtual ulong ReadU64() => BitConverter.ToUInt64(ReadReverse(sizeof(long)), 0);
+
+        private void WriteReverse(byte[] buffer)
+        {
+            Array.Reverse(buffer);
+            WriteBytes(buffer);
+        }
+
+        private byte[] ReadReverse(int count)
+        {
+            var buffer = ReadBytes(count);
+            Array.Reverse(buffer);
+
+            return buffer;
+        }
 
         internal void Pad()
         {
